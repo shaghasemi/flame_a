e7687939c15bc5efc5fb0f025acc55696898d9ae
shaghasemi <shaghasemi@gmail.com>
2022-06-29 18:34:12 +0430

    audio practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GameWidget(game: MyGame()));
+  runApp(
+    GameWidget(
+      // game: MyGame(),
+      game: MyGameAudio(),
+    ),
+  );
 }
+
+final style = TextStyle(color: BasicPalette.white.color);
+final regular = TextPaint(style: style);
 
 class MyGame extends FlameGame with HasTappables {
   final double characterSize = 200;
@@ -188,5 +198,66 @@ class DialogButton extends SpriteComponent with Tappable {
     } catch (ex) {
       return false;
     }
+  }
+}
+
+class MyGameAudio extends FlameGame with TapDetector, DoubleTapDetector {
+  bool isPlayingMusic = false;
+  int trackNumber = 1;
+  TextComponent instructions = TextComponent();
+  final String instructionString = 'Play: Single Tap\n'
+      'Stop: Single Tap\n'
+      'Next Song: Double Tap\n';
+
+  @override
+  Future<void>? onLoad() {
+    instructions
+      ..text = '$instructionString\n Track Number: $trackNumber'
+      ..textRenderer = regular
+      ..y = 100
+      ..x = 20;
+
+    add(instructions);
+    return super.onLoad();
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    if (!isPlayingMusic) {
+      switch (trackNumber) {
+        case 1:
+          FlameAudio.bgm.play('track01.mp3');
+          isPlayingMusic = true;
+          break;
+        case 2:
+          FlameAudio.bgm.play('track02.mp3');
+          isPlayingMusic = true;
+          break;
+      }
+      instructions.text =
+          '$instructionString\n Track Number: $trackNumber\n Status: Playing';
+    } else {
+      FlameAudio.bgm.stop();
+      isPlayingMusic = false;
+      instructions.text =
+          '$instructionString\n Track Number: $trackNumber\n Status: Stopped';
+    }
+    super.onTapUp(info);
+  }
+
+  @override
+  void onDoubleTap() {
+    trackNumber++;
+    if (trackNumber > 2) {
+      trackNumber = 1;
+    }
+    if (isPlayingMusic) {
+      instructions.text =
+          '$instructionString\n Track Number: $trackNumber\n Status: Playing';
+    } else {
+      instructions.text =
+          '$instructionString\n Track Number: $trackNumber\n Status: Stopped';
+    }
+    super.onDoubleTap();
   }
 }
